96015738b1f8c3813ebcd2d11e1bcbd324e55d20
Khojiakbar <harry.gafurov@gmail.com>
2024-03-30 08:58:55 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import {useKindeAuth} from "@kinde-oss/kinde-auth-react";
 import Navbar from "./layout/Navbar";
 function App() {
-
+  const { login, register } = useKindeAuth();
   return (
     <>
       <Navbar/>
-      
+      <button onClick={register} type="button">Register</button>
+      <button onClick={login} type="button">Log In</button>
     </>
   )
 }
